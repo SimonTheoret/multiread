d7c074428b671a13cc6dc5945ed9bbfde3da4b1f
Simon Théorêt <simonteoret@hotmail.com>
2025-09-14 18:00:29 -0400

    WIP: iterator pattern

--- src/lib.rs
+++ src/lib.rs
@@ -63,30 +63,93 @@ impl<const N: usize> IntoIterator for LocationMap<N> {
     }
 }
 
-//NOTE: Change the location map 32 to 16 for m256 ?
-fn parse_slice_lf(s: &[u8]) -> LocationMap<32> {
-    cfg_if! {
-        if #[cfg(all(target_feature = "avx512bw", target_feature = "avx512f"))] {
-            unsafe { find_all_matches_m512(s, b'\n') }
-        } else if #[cfg(target_feature = "avx2")] {
-             unsafe { find_all_matches_m256(s, b'\n') }
-        } else {
-            find_all_matches_fallback(s, b'\n')
+// //NOTE: Change the location map 32 to 16 for m256 ?
+// fn parse_slice_lf_m512(s: &[u8]) -> LocationMap<32> {
+//     cfg_if! {
+//         if #[cfg(all(target_feature = "avx512bw", target_feature = "avx512f"))] {
+//             unsafe { find_all_matches_m512(s, b'\n') }
+//         } else if #[cfg(target_feature = "avx2")] {
+//              unsafe { find_all_matches_m256(s, b'\n') }
+//         } else {
+//             find_all_matches_fallback(s, b'\n')
+//
+//         }
+//     }
+// }
 
+struct LocMapIter<'a> {
+    data: &'a [u8],
+    counter: usize,
+}
+
+impl<'a> Iterator for LocMapIter<'a> {
+    type Item = LocationMap<32>;
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let start = self.counter;
+            if start >= self.data.len() {
+                return None;
+            }
+            let end = start + 32;
+            let mut extended: [u8; 32];
+            let slice = if end <= self.data.len() {
+                &self.data[start..end]
+            } else {
+                let short_slice = &self.data[start..];
+                extended = [0; 32];
+                extended[..short_slice.len()].copy_from_slice(short_slice);
+                &extended
+            };
+            let loc_map = unsafe { find_all_matches_m512(slice, b'\n') };
+            if loc_map.len == 0 {
+                continue;
+            }
+            self.counter += 32;
+            return Some(loc_map);
         }
     }
 }
 
-pub struct LineParser<'a>(&'a [u8]);
-
-impl<'a> LineParser<'a> {
-    const OFFSET_MULTIPLIER: usize = 32;
+pub struct NewLinePositionIter<'a> {
+    data: &'a [u8],
+    iter: LocMapIter<'a>,
+    buf: LocationMap<32>,
+    buf_index: usize,
+}
+impl<'a> NewLinePositionIter<'a> {
+    pub fn new(data: &'a [u8]) -> Self {
+        let buf = LocationMap {
+            map: [0; 32],
+            len: 0,
+        };
+        let iter = LocMapIter { data, counter: 0 };
+        Self {
+            data,
+            iter,
+            buf,
+            buf_index: 0,
+        }
+    }
 }
 
-impl<'a> Iterator for LineParser<'a> {
-    type Item = &'a [u8];
+impl<'a> Iterator for NewLinePositionIter<'a> {
+    type Item = usize;
     fn next(&mut self) -> Option<Self::Item> {
-        parse_slice_lf()
+        if self.buf_index < self.buf.len {
+            let res = Some(self.buf.map[self.buf_index]);
+            self.buf_index += 1;
+            return res;
+        }
+        // Non-emtpy (len >= 1) location map
+        // See LocMapIter.next()
+        if let Some(loc_map) = self.iter.next() {
+            self.buf_index = 0;
+            self.buf = loc_map;
+            let res = self.buf.map[self.buf_index];
+            Some(res)
+        } else {
+            None
+        }
     }
 }
 
@@ -101,12 +164,11 @@ mod test {
             Second sentence
             Third"#
             .as_bytes();
-        let expected_content = vec![
-            "First sentence".as_bytes(),
-            "Second sentence".as_bytes(),
-            "Third".as_bytes(),
-        ];
-        let iter = LineParser(&exemple);
-        assert_eq!(iter.collect(), expected_content);
+        let iter = NewLinePositionIter::new(exemple);
+        let actual_newlines: Vec<_> = iter.collect();
+        dbg!(&actual_newlines);
+        for pos in actual_newlines {
+            assert_eq!(exemple[pos], b'\n')
+        }
     }
 }
