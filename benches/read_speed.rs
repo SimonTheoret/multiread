79d5f3f5d8efd881720b518d81684ff476d8efbe
Simon Théorêt <simonteoret@hotmail.com>
2025-09-13 22:35:11 -0400

    WIP: started benchmarking

---
+++ benches/read_speed.rs
@@ -0,0 +1,6 @@
+use std::hint::black_box;
+use criterion::{criterion_group, criterion_main, Criterion};
+
+// fn criterion_benchmark(c: &mut Criterion) {
+//     c.bench_function("avx-512", |b| b.iter(|| ))
+// }
